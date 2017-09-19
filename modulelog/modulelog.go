443dab166baa6fe30f7fc7f0f2299ad6a0f26e40
Daniele Basile <asterix24@gmail.com>
2017-09-19 21:24:11 +0200

    Send all read package from modules to remote server.

--- modulelog/modulelog.go
+++ modulelog/modulelog.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/jacobsa/go-serial/serial"
 )
 
-func main() {
+type PostMsg struct {
+	Timestamp    time.Time `json:"timestamp"`
+	Address      int       `json:"address"`
+	Description  string    `json:"description"`
+	Label        string    `json:"label"`
+	Lqi          int       `json:"lqi"`
+	Rssi         int       `json:"rssi"`
+	Uptime       int       `json:"uptime"`
+	Tempcpu      int       `json:"tempcpu"`
+	Vrefcpu      int       `json:"vrefcpu"`
+	Ntc0         int       `json:"ntc0"`
+	Ntc1         int       `json:"ntc1"`
+	Photores     int       `json:"photores"`
+	Pressure     int       `json:"pressure"`
+	Temppressure int       `json:"temppressure"`
+}
 
+func main() {
 	// Set up options.
 	options := serial.OpenOptions{
 		PortName:        "/dev/ttyS2",
@@ -22,24 +43,95 @@ func main() {
 	// Open the port.
 	port, err := serial.Open(options)
 	if err != nil {
-		log.Fatalf("serial.Open: %v", err)
+		fmt.Println("serial.Open: %v", err)
 	}
 
 	// Make sure to close it later.
 	defer port.Close()
 
 	for {
-		buf := make([]byte, 100)
-		n, err := port.Read(buf)
-		if err != nil {
-			log.Fatalf("port.Read: %v", err)
+		line := ""
+		for {
+			buf := make([]byte, 1)
+			n, err := port.Read(buf)
+			if err != nil {
+				fmt.Println("port.Read: %v", err)
+			}
+			if string(buf[:n]) == "\n" {
+				break
+			}
+			line = line + string(buf[:n])
 		}
-		line := string(buf[:n])
-		fmt.Print(line)
+
+		var loc *time.Location
+		//set timezone,
+		loc, err = time.LoadLocation("Europe/Rome")
+		if err != nil {
+			fmt.Println("Unable go get time clock..")
+			panic(err)
+		}
+
+		now := time.Now().In(loc)
+		t := now.Format("Mon Jan _2 15:04:05 2006 ")
+		if len(line) > 1 {
+			fmt.Println(t, line)
+		}
 		if strings.HasPrefix(line, "$") {
 			line = line[1:]
-			fields := line.Split(line, ";")
-			fmt.Printf(fields)
+			fields := strings.Split(line, ";")
+
+			var postData PostMsg
+			for item := 0; item < len(fields); item++ {
+				switch item {
+				case 0:
+					postData.Address, _ = strconv.Atoi(fields[item])
+					label := "Slave"
+					if fields[item] == "0" {
+						label = "Master"
+					}
+					postData.Label = label
+					postData.Description = "-"
+					postData.Timestamp = time.Now().In(loc)
+				case 1:
+					postData.Lqi, _ = strconv.Atoi(fields[item])
+				case 2:
+					postData.Rssi, _ = strconv.Atoi(fields[item])
+				case 3:
+					postData.Uptime, _ = strconv.Atoi(fields[item])
+				case 4:
+					postData.Tempcpu, _ = strconv.Atoi(fields[item])
+				case 5:
+					postData.Vrefcpu, _ = strconv.Atoi(fields[item])
+				case 6:
+					postData.Ntc0, _ = strconv.Atoi(fields[item])
+				case 7:
+					postData.Ntc1, _ = strconv.Atoi(fields[item])
+				case 8:
+					postData.Photores, _ = strconv.Atoi(fields[item])
+				case 9:
+					postData.Pressure, _ = strconv.Atoi(fields[item])
+				case 10:
+					postData.Temppressure, _ = strconv.Atoi(fields[item])
+				}
+			}
+
+			jsonData, _ := json.Marshal(postData)
+			req, err := http.NewRequest("POST", "http://radiolog.asterix.cloud/import", bytes.NewBuffer(jsonData))
+
+			//fmt.Println(t, "URL:>", url)
+			//fmt.Println(string(jsonData))
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Print(err)
+				continue
+			}
+			defer resp.Body.Close()
+
+			body, _ := ioutil.ReadAll(resp.Body)
+			fmt.Println(t, resp.Status, string(body))
+
 		}
 	}
 }
